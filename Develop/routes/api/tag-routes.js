81f663462dd79161e35a547361af5790010a66f2
erog-tech <prikshitthakur0@gmail.com>
2022-09-04 20:52:47 +0200

    updated the tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,24 +5,83 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  await Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [{
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      through: "ProductTag",
+    }, ],
+  })
+  .then((parsedTagData) => {
+    res.json(parsedTagData);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      through: "ProductTag",
+    }],
+  })
+  .then((retrievedTag) => {
+    res.json(retrievedTag);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then((tag) => {
+    res.json(tag);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name,
+  },{
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => {
+    res.json(tag);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tagRemoved) => {
+    res.json(`${tagRemoved} tag were removed from the database`);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
